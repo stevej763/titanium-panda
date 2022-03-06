9cee8fb833bac2caad2df97388a4ac3692c86b66
steve <steve.jones763@gmail.com>
2022-03-06 00:55:11 +0000

    Implemented Mongo and comnfigured (badly) integration testing with the db running in a docker container

--- src/test/java/com/titaniumpanda/app/webtest/PhotoResourceWebTest.java
+++ src/test/java/com/titaniumpanda/app/webtest/PhotoResourceWebTest.java
@@ -1,5 +1,6 @@
 package com.titaniumpanda.app.webtest;
 
+import com.titaniumpanda.app.domain.Photo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
@@ -13,12 +14,17 @@ public class PhotoResourceWebTest extends AbstractWebTest {
     private String localhostWithPort;
 
     @BeforeEach
+    @Override
     void setUp() {
         localhostWithPort = "http://localhost:" + port;
+        super.setUp();
     }
 
     @Test
     public void shouldReturnSerializedPhotoDto() {
+        Photo photo = new Photo("photo title", "1", "photoUrl", "description");
+        mongoTemplate.save(photo, "photo");
+
         String url = localhostWithPort + "/api/photo/1";
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
@@ -27,6 +33,13 @@ public class PhotoResourceWebTest extends AbstractWebTest {
 
     @Test
     public void shouldReturnMultipleSerializedPhotoDtos() {
+        Photo photo1 = new Photo("photo title", "1", "photoUrl", "description");
+        Photo photo2 = new Photo("photo title", "2", "photoUrl", "description");
+        Photo photo3 = new Photo("photo title", "3", "photoUrl", "description");
+        mongoTemplate.save(photo1);
+        mongoTemplate.save(photo2);
+        mongoTemplate.save(photo3);
+
         String url = localhostWithPort + "/api/photo/all";
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
         assertThat(responseEntity.getStatusCode(), is(HttpStatus.OK));
